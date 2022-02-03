7e7fd860e43ddea69a52a589cc8b5beba273716b
Twigz101 <alexcarlj@aol.com>
2022-02-02 21:33:28 -0500

    Added visualizer in scene for obstacle avoidance ai

--- ObstacleAvoidanceAI/Assets/BoydMovement.cs
+++ ObstacleAvoidanceAI/Assets/BoydMovement.cs
@@ -5,18 +5,11 @@ using UnityEditor;
 
 public class BoydMovement : MonoBehaviour
 {
-    /*
-    THINGS TO ADD:
-        - Counter for keeping track of how many actual collsions there are
-        - Small circle around the Boyd for even greater accuracy in avoiding obsticles
-        - Walls and other shapes
-        - In Scene visualizers for movement behaviors (visualize rays, wander circle, etc
-        - UI for user controls
-     */
-
     //Boyd Speeds
     [Header("Boyd Speeds")]
-    public float mSpeed = 5.0f;
+    public float mMaxSpeed = 5.0f;
+    float mHalfSpeed;
+    float mCurrentSpeed;
     public float mRotSpeed = 1.0f;
 
     //Obsticle Avoidance Raycast data
@@ -34,8 +27,13 @@ public class BoydMovement : MonoBehaviour
 
     //Wander Data
     [Header("Wander Data")]
-    public float mCircleDist = 5.0f;
-    public float mCircleRadius = 2.0f;
+    public float mWanderCircleDist = 5.0f;
+    public float mWanderCircleRadius = 2.0f;
+
+    //Line Renderer Data
+    [Header("Line Renderer Data")]
+    public float mLineWidth = 0.1f;
+    private LineRenderer mLR;
 
     //Misc Data
     private Rigidbody2D mRB;
@@ -43,7 +41,13 @@ public class BoydMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        mHalfSpeed = mMaxSpeed / 2.0f;
+        mCurrentSpeed = mMaxSpeed;
+
         mRB = GetComponent<Rigidbody2D>();
+        mLR = GetComponent<LineRenderer>();
+
+        InitLineRenderer();
     }
 
     // Update is called once per frame
@@ -68,10 +72,17 @@ public class BoydMovement : MonoBehaviour
             RaycastHit2D currentRay = Physics2D.Raycast(transform.position, newRayDir * mRayDist, mRayDist);
             if (currentRay)
             {
-                Debug.DrawLine(transform.position, currentRay.point, Color.red);
                 Debug.Log("Obsticle Spotted");
 
+                CreateLine(transform.position, newRayDir, mRayDist);
+                mLR.enabled = true;
+
                 newBoydRot = AvoidBehavoir((Vector2)currentRay.collider.transform.position);
+
+                if (mCurrentSpeed > mHalfSpeed)
+                {
+                    mCurrentSpeed -= Time.deltaTime;
+                }
                 break;
             }
         }
@@ -80,10 +91,16 @@ public class BoydMovement : MonoBehaviour
         if (newBoydRot == Quaternion.identity)
         {
             newBoydRot = WanderBehavior();
+
+            if (mCurrentSpeed < mMaxSpeed)
+            {
+                mCurrentSpeed += Time.deltaTime;
+            }
         }
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, newBoydRot, Time.deltaTime * mRotSpeed);
-        mRB.velocity = transform.up * mSpeed;
+        mRB.velocity = transform.up * mCurrentSpeed;
+
     }
 
     //Wander Behavior: Calculate and return rotation towards a random location
@@ -91,18 +108,19 @@ public class BoydMovement : MonoBehaviour
     Quaternion WanderBehavior()
     {
         Debug.Log("Wandering around");
+        mLR.enabled = false;
 
         //Get the center of the circle a fixed distance away from the boyd
         //Algorithm: currentVel.norm * cirlceDist - currentPos = circleCenter
-        Vector2 circleCenter = (((Vector2)mRB.velocity).normalized * mCircleDist) - (Vector2)transform.position;
+        Vector2 circleCenter = (((Vector2)mRB.velocity).normalized * mWanderCircleDist) - (Vector2)transform.position;
 
         // Generate a point on the circle by using a random angle
         // Angle is converted into radian form (angle * pi / 180)
         // That new point will be the target
         float randAngle = Random.Range(0, 360) * Mathf.PI / 180.0f;
 
-        float circlePosX = circleCenter.x + ((Mathf.Cos(randAngle) * mCircleRadius));
-        float circlePosY = circleCenter.y + ((Mathf.Sin(randAngle) * mCircleRadius));
+        float circlePosX = circleCenter.x + ((Mathf.Cos(randAngle) * mWanderCircleRadius));
+        float circlePosY = circleCenter.y + ((Mathf.Sin(randAngle) * mWanderCircleRadius));
         Vector2 randPoint = new Vector2(circlePosX, circlePosY);
 
         //Rotate based on random value on the sphere
@@ -113,9 +131,10 @@ public class BoydMovement : MonoBehaviour
     Quaternion AvoidBehavoir(Vector2 fleeFromPos)
     {
         Debug.Log("Obsticle Spotted");
+        mLR.enabled = true;
 
         //Desired veolocity is based on the current position and the normalized position of the collided object
-        Vector2 desiredVel = ((Vector2)transform.position - fleeFromPos).normalized * mSpeed;
+        Vector2 desiredVel = ((Vector2)transform.position - fleeFromPos).normalized * mCurrentSpeed;
         Vector2 target = desiredVel - mRB.velocity;
 
         //Return rotation based on the new target
@@ -135,5 +154,31 @@ public class BoydMovement : MonoBehaviour
             float newPosY = Mathf.Clamp(transform.position.y, mSCREEN_MAX_Y, mSCREEN_MIN_Y);
             transform.position = new Vector2(transform.position.x, newPosY);
         }
+    }
+
+    //Initialize the line renderer and get it set up for visualizing the rays
+    private void InitLineRenderer()
+    {
+        Vector3[] initLinePos = new Vector3[2] { Vector3.zero, Vector3.zero };
+        mLR.SetPositions(initLinePos);
+
+        mLR.startWidth = mLineWidth;
+        mLR.endWidth = mLineWidth;
+    }
+
+    //Creates a line 
+    void CreateLine(Vector3 start, Vector3 dir, float length)
+    {
+        Ray ray = new Ray(start, dir);
+        RaycastHit raycastHit;
+        Vector3 endPosition = start + (length * dir);
+
+        if (Physics.Raycast(ray, out raycastHit, length))
+        {
+            endPosition = raycastHit.point;
+        }
+
+        mLR.SetPosition(0, start);
+        mLR.SetPosition(1, endPosition);
     }
 }
